0b4ac4416af6acf4a2d6dac827fdc27e37e78d31
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-02-28 10:02:39 -0300

    1-Overview

---
+++ BatchScript/1-Overview.cmd
@@ -0,0 +1,20 @@
+Batch Script is incorporated to automate command sequences
+    Features
+        Can read inputs from users
+        Has control structures
+            for 
+            if
+            while
+            switch
+            (..)
+        Suport Functions and Arrays
+        Regular Expressions
+        Can Include Other programming codes such as Perl
+    Common users of Batch   
+        Setting up servers for different purpoeses
+        Automating housekeeping activities such deleting unwanted files or log files
+        Automating the deployment of applications from one environment to another
+        Installing programs on varios machines at once.
+    Extensions for Batch is BAT or CMD
+        Recognized and executed through an interface called a shell provided by a system file called Command interpreter
+        On Windows system, this interpreter is known as cmd.exe.
