2491c56e985b547985e404e702f84d1fac545940
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-09 11:13:59 -0300

    cd.cmd

---
+++ BatchScript/3-Commands/cd.cmd
@@ -0,0 +1,25 @@
+@echo off
+rem changes to a differente directory, or displays the current diretory
+
+rem Syntax
+cd
+
+rem cd without any parametes display currnte folder
+cd
+
+rem changing the path to Program files
+cd\Program files
+
+Rem changing into user folder
+cd %USERPROFILE%
+
+rem came back one directory
+cd ..
+
+rem Raiz
+cd ..\..
+
+rem Other way
+c:\
+c:\Program files
+c:\users\Administrator
---
+++ conti.cmd
@@ -0,0 +1,2 @@
+rem file:///L:/Linear_/Base-Jump_05/BatchScript.pdf
+rem 17/152
